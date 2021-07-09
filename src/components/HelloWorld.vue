cbb47852fcae7f3ac19fed175d4c8c1d0ff1c4d5
Jhon Nieves <jancx031@gmail.com>
2021-07-09 17:11:51 -0400

    theModal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
     </a>
-    |
+
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
---
+++ src/components/TheModal.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>this is working</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
