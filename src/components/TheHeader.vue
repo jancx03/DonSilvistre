98c83b7e15136fcb249fd24dca30bfdc947d08e0
Miguel Arias <ctdesing@gmail.com>
2021-07-19 14:08:14 -0400

    fontawsome icons

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,13 +8,20 @@
         <BaseButton mode="flat" label="link" />
         <BaseButton mode="flat" label="link" />
       </div>
-      <h1>ICONS</h1>
+      <div>
+        <FontAwsomeIcon class="icon" icon="search" />
+        <FontAwsomeIcon class="icon" icon="user" />
+        <FontAwsomeIcon class="icon" icon="shopping-cart"/>
+      </div>
     </div>
   </header>
 </template>
 
 <script setup>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faShoppingCart, faUser, faSearch } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faShoppingCart, faUser, faSearch);
 </script>
 
 <style scoped>
@@ -32,6 +39,13 @@ header {
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+}
+
+.icon {
+  font-size: 2rem;
+  color: grey;
+  font-weight: 100;
+  margin-right: 2rem;
 }
 
 @media all and (max-width: 768px) {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import BaseButton from '/src/components/BaseButton.vue';
 import TheModal from '/src/components/TheModal.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 const app = createApp(App);
 
 app.component('BaseButton', BaseButton);
 app.component('TheModal', TheModal);
+app.component('FontAwsomeIcon', FontAwesomeIcon)
 app.mount('#app')
