57cb7544e8ee12c0869d77206cfd5b1de5b44d6b
Jhon Nieves <jancx031@gmail.com>
2021-07-20 19:15:59 -0400

    k

--- src/components/BaseGrid.vue
+++ src/components/BaseGrid.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="container">
-    <div class="red">Cool</div>
-    <div class="blue">Cool</div>
-    <div class="green">Cool</div>
+    <div class="red">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo eos
+      amet, debitis non cum officiis placeat temporibus est impedit nam quidem
+      velit nobis esse aliquam. Ullam sit esse atque quidem?
+    </div>
+    <div class="blue">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ullam iste
+      est consequuntur ipsam eius commodi, accusantium magnam doloremque cumque
+      voluptas perspiciatis vero incidunt quas necessitatibus impedit deserunt
+      at maxime.
+    </div>
+    <div class="green">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, illum, vel
+      dolorem deserunt ullam blanditiis id fuga distinctio obcaecati totam, sint
+      optio doloremque hic recusandae minima autem consequuntur tenetur iusto.
+    </div>
   </div>
 </template>
 
@@ -15,6 +28,12 @@ export default {};
   display: flex;
 }
 .red {
+  /* background-color: red; */
 }
-.blue;
+.blue {
+  /* background-color: blue; */
+}
+.green {
+  /* background-color: green; */
+}
 </style>
