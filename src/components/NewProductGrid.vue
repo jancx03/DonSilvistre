03e8a33504a0afe3e4742b88697d58353268eb74
Jhon Nieves <jancx031@gmail.com>
2021-07-22 15:10:56 -0400

    ff

---
+++ src/components/NewProductGrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  grid-template-columns: 100px 50px 100px;
+  grid-template-rows: 80px auto 80px;
+  justify-items: center;
+  column-gap: 10px;
+  row-gap: 15px;
+}
+.image {
+  position: relative;
+  background-image: url("https://images.unsplash.com/photo-1600857544200-b2f666a9a2ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80");
+  width: 240px;
+  height: 120px;
+}
+</style>
