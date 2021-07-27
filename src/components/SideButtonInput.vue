23bc7d128ac0b0b9f40e8c6682ceb20884025e60
Miguel Arias <ctdesing@gmail.com>
2021-07-27 15:08:33 -0400

    'save'

--- src/components/SideButtonInput.vue
+++ src/components/SideButtonInput.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
-    <div class="input-wrapper">
-      <input type="text">
-    </div>
+    <input type="text">
     <BaseButton mode="outline" label="Subscribe"/>
   </div>
 </template>
 
 <style scoped>
-.input-wrapper {
-  border: 1px solid black;
-  padding: 1rem;
-}
 </style>
