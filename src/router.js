079a9d94faa0249cbff29314813e0d5d416f8e9e
Miguel Arias <ctdesing@gmail.com>
2021-07-29 15:32:11 -0400

    Product component mount and products

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TheIndex from '/src/pages/TheIndex.vue';
+import TheProduct from '/src/pages/TheProduct.vue'
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component:  TheIndex }
+    { path: '/', component:  TheIndex },
+    { path: '/products/:id', component: TheProduct, props: true }
   ],
   scrollBehavior: (_to, _from, savedPosition) => {
     return savedPosition? savedPosition : { top: 0, left: 0 };
