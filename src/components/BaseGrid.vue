d4c28c63b7d6020ce1d06096bebdb463eaef8f9f
Jhon Nieves <jancx031@gmail.com>
2021-08-03 17:48:31 -0400

    cf

--- src/components/BaseGrid.vue
+++ src/components/BaseGrid.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="container">
+    <svg
+      class="small"
+      aria-hidden="true"
+      focusable="false"
+      role="presentation"
+      viewBox="0 0 41 35"
+    >
+      <path
+        d="M10.208 17.711h6.124v16.332H0V21.684C0 8.184 5.444.956 16.332 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766zm24.498 0h6.124v16.332H24.498V21.684C24.498 8.184 29.942.956 40.83 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766z"
+        fill="#000"
+        fill-rule="evenodd"
+      ></path>
+    </svg>
     <div class="red">
       <p>Best body oil, love the fragrance, very hydrating and calming.</p>
     </div>
     <div class="blue">
+      <svg
+        class="small"
+        aria-hidden="true"
+        focusable="false"
+        role="presentation"
+        viewBox="0 0 41 35"
+      >
+        <path
+          d="M10.208 17.711h6.124v16.332H0V21.684C0 8.184 5.444.956 16.332 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766zm24.498 0h6.124v16.332H24.498V21.684C24.498 8.184 29.942.956 40.83 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766z"
+          fill="#000"
+          fill-rule="evenodd"
+        ></path>
+      </svg>
       <p>
         Best body lotion! Absorbs fast and leaves skin feeling soft and
         hidrated. Plus smells AMAZING!
       </p>
     </div>
     <div class="green">
+      <svg
+        class="small"
+        aria-hidden="true"
+        focusable="false"
+        role="presentation"
+        viewBox="0 0 41 35"
+      >
+        <path
+          d="M10.208 17.711h6.124v16.332H0V21.684C0 8.184 5.444.956 16.332 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766zm24.498 0h6.124v16.332H24.498V21.684C24.498 8.184 29.942.956 40.83 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766z"
+          fill="#000"
+          fill-rule="evenodd"
+        ></path>
+      </svg>
       <p>My skin loves it, so gentle, effective and oh so natural 💚</p>
     </div>
   </div>
@@ -31,5 +70,8 @@ p {
   font-size: 1.10938em;
   font-weight: 400;
   font-style: normal;
+}
+.small {
+  width: 1.4rem;
 }
 </style>
